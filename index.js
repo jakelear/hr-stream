5a07bf7b3425f271ccfbe459f080004cbbcc0d57
Jake Lear <jake.lear@voxmedia.com>
2017-01-29 18:15:07 -0500

    reorganized index

--- index.js
+++ index.js
@@ -1,111 +1,145 @@
-window.addEventListener('load', init, false);
+var HRStream = {
+  poll_timing: 1500,
+  current_rate: 60,
+  min_rate: 50,
+  max_rate: 150,
+  pulse_anim_rate: 700,
+  graph_height: 300,
+  graph_width: 600,
+  graph_color: '#FFF',
 
-var current_rate = 60;
-var min_rate = 50;
-var max_rate = 150;
+  /**
+  * init
+  * Fired on page load to kick off update loop, pulse animation, and line graph
+  */
+  init: function() {
+    HRStream.updateRate();
+    HRStream.pulse();
+    HRStream.renderGraph();
+  },
 
-function init() {
-  doPoll();
-}
+  /**
+  * updateRate
+  * gets the current rate from rate.json
+  * updates the text and the current_rate var (which is used for the line graph)
+  */
+  updateRate: function() {
+    $.get('rate.json', function(data) {
+      HRStream.current_rate = data.rate;
+      $('#heartrate p').text(HRStream.current_rate);
+      setTimeout(HRStream.updateRate, HRStream.poll_timing);
+    });
+  },
 
-function doPoll(){
-  $.get('rate.json', function(data) {
-      current_rate = data.rate;
-      $('#heartrate p').text(current_rate);
-      setTimeout(doPoll,1500);
-  });
-}
-
-animation_rate = 700;
-animation_rate = 700 - (current_rate * 4);
-animation_rate = animation_rate < 200 ? 200 : animation_rate;
-(function pulse(back) {
+  /**
+  * pulse
+  * Animates the heart icon to pulse at varying speeds
+  * Speeds up based on current_rate to a max of every 200ms
+  */
+  pulse: function(back) {
+    var animation_rate = HRStream.pulse_anim_rate;
+    animation_rate = HRStream.pulse_anim_rate - (HRStream.current_rate * 4);
+    // If hr gets high enough that anim rate drops below 200, peg it at 200
+    animation_rate = animation_rate < 200 ? 200 : animation_rate;
     var $heart = $('.heart');
-    $('.heart').animate({
+    $heart.animate({
       width: (back) ? $heart.width() + 2 : $heart.width() - 2
-    }, animation_rate, function(){pulse(!back)});
-})(false);
+    }, animation_rate, function(){HRStream.pulse(!back)});
+  },
 
-var limit = 120,
-    duration = 750,
-    now = new Date(Date.now() - duration)
+  /**
+  * renderGraph
+  * sets up a basic d3 streaming line graph
+  */
+  renderGraph: function (){
+    var limit = 120,
+        duration = 750,
+        now = new Date(Date.now() - duration)
 
-var width = 600,
-    height = 300
+    var width = HRStream.graph_width,
+        height = HRStream.graph_height;
 
-var series = {
-  value: 0,
-  color: 'white',
-  data: d3.range(limit).map(function() {
-      return min_rate;
-  })
-}
+    var series = {
+      value: 0,
+      color: HRStream.graph_color,
+      data: d3.range(limit).map(function() {
+          return HRStream.min_rate;
+      })
+    }
 
-var x = d3.time.scale()
-    .domain([now - (limit - 2), now - duration])
-    .range([0, width])
+    var x = d3.time.scale()
+        .domain([now - (limit - 2), now - duration])
+        .range([0, width])
 
-var y = d3.scale.linear()
-    .domain([min_rate, max_rate])
-    .range([height + min_rate, min_rate])
+    var y = d3.scale.linear()
+        .domain([HRStream.min_rate, HRStream.max_rate])
+        .range([height + HRStream.min_rate, HRStream.min_rate])
 
-var line = d3.svg.line()
-    .interpolate('basis')
-    .x(function(d, i) {
-      return x(now - (limit - 1 - i) * duration)
-    })
-    .y(function(d) {
-      return y(d)
-    })
+    var line = d3.svg.line()
+        .interpolate('basis')
+        .x(function(d, i) {
+          return x(now - (limit - 1 - i) * duration)
+        })
+        .y(function(d) {
+          return y(d)
+        })
 
-var svg = d3.select('#canvas').append('svg')
-    .attr('class', 'chart')
-    .attr('width', width)
-    .attr('height', height + (min_rate * 2))
+    var svg = d3.select('#canvas').append('svg')
+        .attr('class', 'chart')
+        .attr('width', width)
+        .attr('height', height + (HRStream.min_rate * 2))
 
-//Container for the gradients
-var defs = svg.append("defs");
+    //Container for the gradients
+    var defs = svg.append("defs");
 
-// Filter for the outside glow
+    // Filter for the outside glow
 
-var filter = defs.append("filter")
-    .attr("id","glow");
-filter.append("feGaussianBlur")
-    .attr("stdDeviation","3.5")
-    .attr("result","coloredBlur");
-var feMerge = filter.append("feMerge");
-feMerge.append("feMergeNode")
-    .attr("in","coloredBlur");
-feMerge.append("feMergeNode")
-    .attr("in","SourceGraphic");
+    var filter = defs.append("filter")
+        .attr("id","glow");
+    filter.append("feGaussianBlur")
+        .attr("stdDeviation","3.5")
+        .attr("result","coloredBlur");
+    var feMerge = filter.append("feMerge");
+    feMerge.append("feMergeNode")
+        .attr("in","coloredBlur");
+    feMerge.append("feMergeNode")
+        .attr("in","SourceGraphic");
 
-var paths = svg.append('g')
+    var paths = svg.append('g')
 
-series.path = paths.append('path')
-  .data([series.data])
-  .attr('class', 'series-path')
-  .style('stroke', series.color)
-  .style("filter", "url(#glow)")
+    series.path = paths.append('path')
+      .data([series.data])
+      .attr('class', 'series-path')
+      .style('stroke', series.color)
+      .style("filter", "url(#glow)")
 
-function tick() {
-now = new Date();
-  // Add new values
+    function tick() {
+      now = new Date();
+      // Add new values
+      series.data.push(HRStream.current_rate)
+      series.path.attr('d', line)
 
-  series.data.push(current_rate)
-  series.path.attr('d', line)
+      // Shift domain
+      x.domain([now - (limit - 2) * duration, now - duration])
 
-  // Shift domain
-  x.domain([now - (limit - 2) * duration, now - duration])
+      // Slide paths left
+      // When slide is done, fire tick again
+      paths.attr('transform', '')
+          .transition()
+          .duration(duration)
+          .ease('linear')
+          .attr('transform', 'translate(' + x(now - (limit - 1) * duration) + ')')
+          .each('end', tick)
 
-  // Slide paths left
-  paths.attr('transform', null)
-      .transition()
-      .duration(duration)
-      .ease('linear')
-      .attr('transform', 'translate(' + x(now - (limit - 1) * duration) + ')')
-      .each('end', tick)
+      series.data.shift();
+    }
 
-  series.data.shift();
-}
+    tick();
+  }
 
-tick();
+};
+
+/**
+* Run the init on page load
+*/
+window.addEventListener('load', HRStream.init, false);
